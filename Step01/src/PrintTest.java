83f8e3606f0cff6158b44e1efcfc30a60526ce18
nam2626 <nam2626@naver.com>
2023-05-08 14:44:57 +0900

    출력 및 단축키

---
+++ Step01/src/PrintTest.java
@@ -0,0 +1,22 @@
+
+public class PrintTest {
+	/*
+	 * 주석은 코드에 설명문을 작성하는 영역
+	 * 실행이 안되는 코드
+	 */
+	public static void main(String[] args) {
+		System.out.println("남상규");
+		System.out.print("남상규\n");
+		System.out.println("서울시 강서구 화곡동");
+
+		System.out.println("Hello World - 1");
+		// 한줄 단위 주석
+//		System.out.println("Hello World - 2");
+		System.out.println("Hello World - 3");
+
+		System.out.println("Hello World - 4");
+		System.out.println("Hello World - 5");
+
+	}
+
+}
