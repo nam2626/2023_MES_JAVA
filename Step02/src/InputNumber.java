d055831d15fca95fe833ed3c5cfb953c71c92ca5
nam2626 <nam2626@naver.com>
2023-05-09 11:17:39 +0900

    숫자 입력

---
+++ Step02/src/InputNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class InputNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 입력 : ");
+		//입력 받음
+		int n = sc.nextInt();//숫자를 입력 받은 것을 n에 저장
+		System.out.println(n);
+		
+	
+	}
+
+}
