14dddc495a0781943c5575db4f754d9087cfafc9
nam2626 <nam2626@naver.com>
2023-05-09 09:18:55 +0900

    사칙연산자

---
+++ Step02/src/Operator1.java
@@ -0,0 +1,38 @@
+
+public class Operator1 {
+	/*
+	 * 	사칙연산자
+	 * 		+	덧셈
+	 * 		-	뺄셈
+	 * 		*	곱셈
+	 * 		/	나눗셈
+	 * 		%	나머지
+	 */
+	public static void main(String[] args) {
+		//정수형 변수 2개 선언 후 임의의 값으로 초기화
+		int n1=10, n2=7;
+		int result = 0;
+		//n1과 n2의 덧셈 결과를 result에 저장, 이때 n1과 n2의 값은 안바뀐다.
+		result = n1 + n2;
+		System.out.println(result);
+		result = n1 - n2;
+		System.out.println(result);
+		result = n1 * n2;
+		System.out.println(result);
+		result = n1 / n2;//몫
+		System.out.println(result);
+		result = n1 % n2;//나머지
+		System.out.println(result);
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
