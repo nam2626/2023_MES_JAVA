9d1a7f2878ce8fe747d50daab1dfc29badf7ada9
nam2626 <nam2626@naver.com>
2023-05-10 17:27:54 +0900

    while 문

--- Step03/src/Quest7.java
+++ Step03/src/Quest7.java
@@ -5,7 +5,14 @@ public class Quest7 {
 	 * 		단, 4와 7의 공배수는 1번만 출력
 	 */
 	public static void main(String[] args) {
-		
+		int i = 1;
+
+		while (i < 101) {
+			if(i % 4 == 0 || i % 7 == 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
 	}
 
 }
