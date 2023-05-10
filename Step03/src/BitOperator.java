148941b9afbd2ff829612c9509b6d13dd239c497
nam2626 <nam2626@naver.com>
2023-05-10 09:37:07 +0900

    비트 연산자

--- Step03/src/BitOperator.java
+++ Step03/src/BitOperator.java
@@ -10,6 +10,34 @@ public class BitOperator {
 		System.out.println(n1 & n2);//0101 -> 5
 		//bit or 연산자 - 둘중에 하나라도 1이면 1이 되는 연산자
 		System.out.println(n1 | n2);//1111 -> 15 -> F
+		//bit xor 연산자 - 서로 값이 다를때만 1
+		System.out.println(n1 ^ n2);//1010 -> 10 -> A
+		//bit not 연산자 - 1과 0을 뒤집음
+		// n1 = 1111 1111 1111 1111 1111 1111 1111 0010
+		//      1000 0000 0000 0000 0000 0000 0000 1110
+		System.out.println(~n1);//-14
+		
+		//l-shift operator
+		System.out.println(n1 << 1);//26
+		System.out.println(n1 << 2);//52		
+		System.out.println(n1 << 3);//104
+		
+		//r-shift operator
+		System.out.println(n1 >> 1);//6
+		System.out.println(n1 >> 2);//3		
+		System.out.println(n1 >> 3);//1
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
