806dbb882b2ff227c36f17bfcf3106725767672f
nam2626 <nam2626@naver.com>
2023-05-10 09:42:59 +0900

    if문

--- Step03/src/IfEx.java
+++ Step03/src/IfEx.java
@@ -15,7 +15,20 @@ public class IfEx {
 	 */
 	
 	public static void main(String[] args) {
-		int n = -10;
+		int n = 10;
+		
+		if(n < 0) {
+			System.out.println("음수 입니다.");
+		}
+		
+		if(n > 0) {
+			System.out.println("양수 입니다.");
+		}
+		
+		if(n == 0) {
+			System.out.println("0 입니다.");
+		}
+		
 	}
 
 }
