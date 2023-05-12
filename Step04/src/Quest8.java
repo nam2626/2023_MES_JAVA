1a617d7e2a4b7754b92a5229840da30d2b6d55ae
nam2626 <nam2626@naver.com>
2023-05-12 09:42:22 +0900

    컨티뉴 추가

--- Step04/src/Quest8.java
+++ Step04/src/Quest8.java
@@ -23,17 +23,18 @@ public class Quest8 {
 			//1. 평점 입력
 			System.out.print("이번 영화의 평점을 입력 하세요 : ");
 			int score = sc.nextInt();
-			if(score >= 1 && score <= 5) {
-				//2. 평점이 1~5면 별 출력하고 종료
-				String star = "";
-				for(int i=0;i<score;i++) {
-					star += "★";
-				}
-				System.out.println("평점 : " +  star);
-				break;
+			if(score < 1 || score > 5) {
+				//2. 평점이 1~5가 아니면 다시 입력
+				System.out.println("평점은 1~5 사이만 입력할 수 있습니다.");
+				continue;
 			}
-			//3. 평점이 1~5가 아니면 다시 입력
-			System.out.println("평점은 1~5 사이만 입력할 수 있습니다.");
+			//3. 평점이 1~5면 별 출력하고 종료
+			String star = "";
+			for(int i=0;i<score;i++) {
+				star += "★";
+			}
+			System.out.println("평점 : " +  star);
+			break;
 		}
 	}
 
