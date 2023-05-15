6f1537ad5da3f49f51c736b06d6d5ca80695a377
nam2626 <nam2626@naver.com>
2023-05-15 11:46:13 +0900

    문제

--- Step06/src/Quest4.java
+++ Step06/src/Quest4.java
@@ -15,15 +15,29 @@ public class Quest4 {
 		Scanner sc = new Scanner(System.in);
 		int w, d, l;//승 무 패 저장할 변수
 		w = d = l = 0;
-		
+		String[] str = {"바위","가위","보"};
 		for(int i = 0;i < 5; i++) {
 			System.out.println( "------ "+ (i+1) + " 회차 ------");
 			System.out.print("가위 - 2, 바위 - 1, 보 - 3 입력 하세요 : ");
 			int player = sc.nextInt();
 			int com = (int)(Math.random() * 3) + 1;
-		}
-		
-	}
+			
+			if(player % 3 + 1 == com) {
+				System.out.println("사용자 : "+str[player-1] +
+						", 컴퓨터 : "+str[com-1] +" 사용자가 이겼습니다.");
+				w++;
+			}else if(player == com) {
+				System.out.println("사용자 : "+str[player-1] +
+						", 컴퓨터 : "+str[com-1] +" 비겼습니다.");
+				d++;				
+			}else {
+				System.out.println("사용자 : "+str[player-1] +
+						", 컴퓨터 : "+str[com-1] +" 컴퓨터가 이겼습니다.");
+				l++;
+			}//end - if
+		}//end - for
+		System.out.println("승 : "+w + ", 무 : " + d +", 패 : " + l);
+	}//end - main
 
 }
 
