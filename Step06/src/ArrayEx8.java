66b2db71710c7c57a1232565899d65dac2ce7e80
nam2626 <nam2626@naver.com>
2023-05-15 14:51:59 +0900

    배열

--- Step06/src/ArrayEx8.java
+++ Step06/src/ArrayEx8.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayEx8 {
@@ -21,8 +22,35 @@ public class ArrayEx8 {
 			if(arr[idx] == -1) break;
 			
 			idx++;
+		}//end - while
+		
+		System.out.println("현재 입력된 내용 : " + Arrays.toString(arr));
+		//1. 삭제할 숫자를 입력 받음
+		System.out.print("삭제할 숫자를 입력하세요 : ");
+		int n = sc.nextInt();
+		//반복문
+		for(int i=0;i<idx;i++) {
+			//	2. 삭제할 숫자가 있는 배열 인덱스를 찾음
+			if(arr[i] == n) {
+				for(int j=i;j<idx-1;j++) {
+				//	3. 배열의 내용을 하나씩 땡겨오는 작업
+					arr[j] = arr[j+1];
+				}
+				//  4. 입력 인덱스를 제어하는 변수를 한번 감소
+				idx--;
+			}
 		}
-
-	}
+		//5. 배열 내용을 전부 출력
+		System.out.println("삭제 결과 : " + Arrays.toString(arr));
+	}//end - main
 
 }
+
+
+
+
+
+
+
+
+
