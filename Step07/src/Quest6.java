098bfed1cd0373752241a20e186d2f903b7c91aa
nam2626 <nam2626@naver.com>
2023-05-16 14:29:49 +0900

    문제

--- Step07/src/Quest6.java
+++ Step07/src/Quest6.java
@@ -5,26 +5,27 @@ public class Quest6 {
 		int[][] arr = new int[5][5];
 		int count = 5;//채우는 개수
 		int row = 0, col = 0;//가로 세로 인덱스 번호
-		int num = 1;
+		int num = 1, sw = 1;
 		while(true) {
 			//가로로 채우기
 			for(int i=0;i<count;i++) {
 				arr[row][col] = num++;
-				col += 1;
+				col += sw;
 			}
 			count--; //가로로 채운 후 채우는 개수가 감소
 			
 			if(count == 0) break; //더 이상 채울 값이 없을때
 			
 			//세로로 채우기
-			col -= 1;
-			row += 1;
+			col -= sw;
+			row += sw;
 			for(int i=0;i<count;i++) {
 				arr[row][col] = num++;
-				row += 1;
+				row += sw;
 			}
-			col -= 1;
-			row -= 1;
+			col -= sw;
+			row -= sw;
+			sw = -sw;//인덱스 번호 증감하는 변수의 부호 변경 
 		}
 		for(int i=0;i<arr.length;i++) {
 			for(int n : arr[i]) {
