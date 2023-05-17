85ec069e503bec94ba3f5a014686bc94db16861b
nam2626 <nam2626@naver.com>
2023-05-17 11:13:46 +0900

    삽입 정렬

---
+++ Step07/src/InsertSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class InsertSort {
+
+	public static void main(String[] args) {
+		int[] arr = new int[6];
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("숫자 입력 : ");
+			arr[i] = sc.nextInt();
+
+			// 정렬
+			for (int j = i; arr[j] < arr[j-1] && j > 0 ; j--) {
+				int temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+			}
+
+			// 정렬되는 과정 출력
+			for (int n : arr) {
+				System.out.printf("%2d ", n);
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+
+
+
+
