4e29b83af43e2e4ea771ab41318a712153ebfa63
nam2626 <nam2626@naver.com>
2023-05-17 10:13:26 +0900

    선택정렬

--- Step07/src/SelectSort.java
+++ Step07/src/SelectSort.java
@@ -7,16 +7,19 @@ public class SelectSort {
 		//1. 해당 작업을 총 배열 개수 - 1 만큼 수행
 		//2. 0번 인덱스부터 ~ 마지막 인덱스 전까지 기준 값으로 잡음
 		for (int i = 0; i < arr.length - 1; i++) {
-			//3. 기준값 다음 인덱스부터 ~ 마지막 인덱스까지 기준값이랑 비교
+			//3. 기준값 다음 인덱스부터 ~ 마지막 인덱스까지 최소값
+			int min = i;
 			for (int j = i+1; j < arr.length; j++) {
-				//4. 비교시 작은 값이 기준값이 있는 위치로 가게끔 데이터 값을 교환
-				if(arr[i] > arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
+				if(arr[min] > arr[j]) {
+					min = j;
 				}
 			}
-			
+			//4. 기준값과 제일 작은 값을 교환
+			if(min != i) {
+				int temp = arr[min];
+				arr[min] = arr[i];
+				arr[i] = temp;
+			}
 		}
 		
 		for(int n : arr) {
