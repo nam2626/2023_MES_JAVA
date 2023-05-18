d6d27ec2f175d7fc126309111286eae2755ff024
nam2626 <nam2626@naver.com>
2023-05-18 15:44:51 +0900

    클래스 예제

---
+++ Step08/src/method/TV.java
@@ -0,0 +1,16 @@
+package method;
+/*
+ *		TV를 동작시키는 클래스
+ *			- 필드 : 채널, 음량, 전원, 음소거
+ *						3			4			1        2
+ *			- 기능 : 채널Up/Down, 음량Up/Down, 전원OnOff, 음소거OnOff
+ *
+ *			채널 : 1~456, 음량 : 0~30, 전원 : true/false, 음소거 : true/false  
+ * 
+ * 		기본 생성자만 생성 - 초기값은 알맞게 임의값으로 초기화
+ * 
+ * 		TV 테스트 하는 클래스 TVMain도 생성해서 기능을 만들때마다 테스트하면 구현
+ */
+public class TV {
+	
+}
