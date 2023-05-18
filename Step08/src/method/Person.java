f6df7e5388aa1bfbfd4e7d38b3cbdfdcfaebf858
nam2626 <nam2626@naver.com>
2023-05-18 10:24:31 +0900

    메서드

--- Step08/src/method/Person.java
+++ Step08/src/method/Person.java
@@ -22,6 +22,13 @@ public class Person {
 		this.gender = gender;
 	}
 	
+	//사용자 정보를 초기화 하는 메서드 - 이름, 나이, 성별을 받아서 필드에 저장
+	void init(String name, int age, boolean gender) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+	
 	//인적정보 출력하는 기능 -> 이름 나이 남자/여자
 	void printPersonInfo() {
 		System.out.println(name + " " + age + " " + (gender ? "남자" : "여자"));
--- Step08/src/method/PersonMain.java
+++ Step08/src/method/PersonMain.java
@@ -6,6 +6,8 @@ public class PersonMain {
 		//Person 생성
 		Person p = new Person("김철수", 22, true);
 		p.printPersonInfo();
+		p.init("홍길동", 32, true);
+		p.printPersonInfo();
 		
 		Person s = new Person("김영희", 33, false);
 		s.printPersonInfo();
