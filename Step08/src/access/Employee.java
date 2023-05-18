2d2ce799c8073aefeb862857e4774181e1e2361b
nam2626 <nam2626@naver.com>
2023-05-18 12:50:26 +0900

    접근 제어자

--- Step08/src/access/Employee.java
+++ Step08/src/access/Employee.java
@@ -1,14 +1,23 @@
 package access;
-
+/*
+ * 	접근제어자
+ * 		변수와 메서드를 접근할 수 있는 범위를 설정하는 옵션
+ *
+ * 		private : 해당 클래스 내에서만 접근이 가능
+ * 		protected : 해당 클래스를 상속받은 자식 클래스들까지 접근이 가능
+ * 		default : 접근 제어자를 사용을 안했을때 자동으로 붙음, 같은 패키지 내에 있는 클래스들까지 접근이 가능
+ * 		public : 누구나 접근이 가능
+ * 
+ */
 public class Employee {
 	//사번
-	String employeeNo;
+	private String employeeNo;
 	//이름
-	String employeeName;
+	private String employeeName;
 	//급여
-	int salary;
+	private int salary;
 	//부서명
-	String department;
+	private String department;
 
 	//전체 초기화하는 생성자
 	public Employee(String employeeNo, String employeeName, int salary, String department) {
@@ -19,6 +28,22 @@ public class Employee {
 		this.department = department;
 	}
 	
+	//이름을 문자열로 받아서 employeeName에 저장
 	
+	
+	
+	//사원정보 출력하는 메서드
+	public void printEmployeeInfo() {
+		System.out.println(employeeNo + " " + employeeName + " "
+											+ salary + " " + department);
+	}
 	
 }
+
+
+
+
+
+
+
+
---
+++ Step08/src/access/EmployeeMain.java
@@ -0,0 +1,15 @@
+package access;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee("A0001", "홍길동", 2300, "경리부");
+		emp.printEmployeeInfo();
+//		emp.employeeName = "김철수";
+		
+//		emp.salary = -3000;
+		emp.printEmployeeInfo();
+		
+	}
+
+}
