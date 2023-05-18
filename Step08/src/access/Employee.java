69f3cd0cd36ad10c2def91874817b92add644274
nam2626 <nam2626@naver.com>
2023-05-18 14:24:07 +0900

    setter/getter

--- Step08/src/access/Employee.java
+++ Step08/src/access/Employee.java
@@ -39,6 +39,36 @@ public class Employee {
 		System.out.println(employeeNo + " " + employeeName + " "
 											+ salary + " " + department);
 	}
+
+	public String getEmployeeNo() {
+		return employeeNo;
+	}
+
+	public void setEmployeeNo(String employeeNo) {
+		this.employeeNo = employeeNo;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	
+	
 	
 }
 
