a639951d19a2e8027cfb9adca846703cf8f5edfc
nam2626 <nam2626@naver.com>
2023-05-19 12:24:11 +0900

    static

---
+++ Step09/src/static_ex/CardMain.java
@@ -0,0 +1,17 @@
+package static_ex;
+
+public class CardMain {
+
+	public static void main(String[] args) {
+		Card c1 = new Card("홍길동동");
+		Card c2 = new Card("김철수");
+		Card c3 = new Card("이영희");
+		Card c4 = new Card("박영수");
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+	}
+
+}
