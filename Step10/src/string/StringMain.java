ce56bb4f3d95af3e9875efa38f356110bb69077f
nam2626 <nam2626@naver.com>
2023-05-22 17:41:39 +0900

    문자열 클래스

--- Step10/src/string/StringMain.java
+++ Step10/src/string/StringMain.java
@@ -24,6 +24,32 @@ public class StringMain {
 		//문자열 끝에서부터 검색 - 검색 결과는 인덱스 번호, 없으면 -1
 		System.out.println(str2.lastIndexOf("World"));
 		System.out.println(str2.lastIndexOf("World",22));
+		//알파벳을 전부 소문자로 변환
+		System.out.println(str2.toLowerCase());
+		//알파벳을 전부 대문자로 변환
+		System.out.println(str2.toUpperCase());
+		//문자열 길이 리턴
+		System.out.println(str2.length());
+		//10번 인덱스부터 마지막까지 문자열을 잘라내서 리턴
+		System.out.println(str2.substring(10));
+		//10번 인덱스부터 15인덱스 전까지 문자열을 잘라내서 리턴
+		System.out.println(str2.substring(10,15));
+		//문자열 바꾸기
+		System.out.println(str2.replace("World", "Hell"));
+		
+		String str3 = "       Hello World      ";
+		System.out.println(str3.length());
+		System.out.println(str3);
+		//문자열 양쪽에 필요없는 공백을 제거
+		System.out.println(str3.trim());
+		System.out.println(str3.trim().length());
+		
+		String str4 = "java,html,css,javascript,android";
+		String[] result = str4.split(",");
+		System.out.println(Arrays.toString(result));
+		
+		
+		
 	}
 
 }
