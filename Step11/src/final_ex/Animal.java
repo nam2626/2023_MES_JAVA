a8d0e5ace8501b8042c14b18199c5aedf6a970ad
nam2626 <nam2626@naver.com>
2023-05-23 12:42:53 +0900

    상속

---
+++ Step11/src/final_ex/Animal.java
@@ -0,0 +1,15 @@
+package final_ex;
+
+public class Animal {
+	private final int age; //이럴 경우 반드시 생성자에서 초기화가 필요
+
+	public Animal(int age) {
+		super();
+		this.age = age;
+	}
+	
+	public void eat() {
+		System.out.println("동물이 먹이를 먹습니다.");
+	}
+	
+}
--- Step11/src/instance_of/InstanceOfMain.java
+++ Step11/src/instance_of/InstanceOfMain.java
@@ -3,7 +3,30 @@ package instance_of;
 public class InstanceOfMain {
 
 	public static void main(String[] args) {
+		Person p = new Person();
+		Animal a = p;
+		Person s = (Person) a;//큰 개념이 작은 개념이 될떄는 강제로 형변환
+		//Dog d = (Dog) a;
+		
+		//A instanceof B ---> 객체 A가 B클래스 타입으로 형변환이 되냐?
+		System.out.println(p instanceof Person);
+		System.out.println(a instanceof Dog);
+		System.out.println(a instanceof Person);
+		System.out.println(p instanceof Animal);
+		
+		//클래스 이름 뽑아서 처리
+		System.out.println(a.getClass().getName());
+		System.out.println(p.getClass().getName());
+		System.out.println(p.getClass().getSimpleName());
+		System.out.println(Person.class.getName());
 		
 	}
 
 }
+
+
+
+
+
+
+
