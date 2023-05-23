fe84758a21ce8bfdfbfff8735f1566380648dc03
nam2626 <nam2626@naver.com>
2023-05-23 16:18:15 +0900

    상속

---
+++ Step11/src/has_a_3/PowerOnOff.java
@@ -0,0 +1,33 @@
+package has_a_3;
+/*
+ * 전원 모듈에서는 전원 값을 저장할 변수
+ * 전원을 켜고, 끄는 메서드, 전원OnOff하는 메서드 총 3가지 메서드를 작성
+ */
+public class PowerOnOff {
+	private boolean power;
+	
+	public void powerOn() {
+		power = true;
+		System.out.println("전원 On");
+	}
+	public void powerOff() {
+		power = false;
+		System.out.println("전원 Off");
+	}
+	
+	public void powerOnOff() {
+		power = !power;
+		if(power)
+			System.out.println("전원 On");
+		else
+			System.out.println("전원 Off");
+			
+	}
+	
+}
+
+
+
+
+
+
