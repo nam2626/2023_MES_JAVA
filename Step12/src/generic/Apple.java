3f029f81aa3665f2e2d7abe6f127e57008d8dd8e
nam2626 <nam2626@naver.com>
2023-05-24 15:40:06 +0900

    제네릭

---
+++ Step12/src/generic/Apple.java
@@ -0,0 +1,7 @@
+package generic;
+
+public class Apple {
+	public void printApple() {
+		System.out.println("사과 입니다.");
+	}
+}
---
+++ Step12/src/generic/Orange.java
@@ -0,0 +1,7 @@
+package generic;
+
+public class Orange {
+	public void printOrange() {
+		System.out.println("오렌지 입니다.");
+	}
+}
