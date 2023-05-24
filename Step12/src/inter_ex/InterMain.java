1808801414fc4a39232f7c610b4b0be5147241ec
nam2626 <nam2626@naver.com>
2023-05-24 10:50:21 +0900

    인터페이스

---
+++ Step12/src/inter_ex/InterMain.java
@@ -0,0 +1,11 @@
+package inter_ex;
+
+public class InterMain {
+
+	public static void main(String[] args) {
+		InterClass interClass = new InterClass();
+		interClass.interA();
+		System.out.println(interClass.circleArea(4.2));
+	}
+
+}
