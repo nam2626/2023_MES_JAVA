c1546387ea0e1af862b74344ac0af950aa96b020
nam2626 <nam2626@naver.com>
2023-05-24 11:23:12 +0900

    인터페이스

--- Step12/src/inter_ex/InterMain.java
+++ Step12/src/inter_ex/InterMain.java
@@ -6,6 +6,10 @@ public class InterMain {
 		InterClass interClass = new InterClass();
 		interClass.interA();
 		System.out.println(interClass.circleArea(4.2));
+		
+		//인터페이스도 변수 타입으로 선언이 가능, 단 직접적인 생성 X
+		InterA interA = interClass;
+		interA = new InterClassB();
 	}
 
 }
