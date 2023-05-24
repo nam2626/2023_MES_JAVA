d439a85713ba74450f507cfde78c92d45803c7e2
nam2626 <nam2626@naver.com>
2023-05-24 14:18:54 +0900

    학생정보 추가

--- StudentProject_Ver2/src/service/StudentService.java
+++ StudentProject_Ver2/src/service/StudentService.java
@@ -35,7 +35,6 @@ public class StudentService {
 	//학생정보 추가하는 작업
 	public void insertStudentVO(Scanner sc) {
 		System.out.println("학생정보 등록을 시작합니다......");
-		//1. 학생정보 입력
 		System.out.print("학번 : ");
 		String studentNo = sc.nextLine();
 		System.out.print("이름 : ");
@@ -45,19 +44,8 @@ public class StudentService {
 		System.out.print("평점 : ");
 		double score = sc.nextDouble();
 		sc.nextLine();
-		//2. StudentVO 객체 생성
-		StudentVO vo = new StudentVO(studentNo, studentName, major, score);
-		//3. 배열의 빈요소에 저장
-		//	3-1. 배열에 빈공간이 없다면?
-		//       배열의 크기를 늘려주고 저장
-		if(idx == arr.length) {
-			StudentVO[] temp = new StudentVO[arr.length+3];
-			//System.arraycopy(복사할 원본배열,복사를 시작할 인덱스번호, 복사할 곳 배열, 복사할 인덱스번호,복사받을 값의 개수);
-			System.arraycopy(arr, 0, temp, 0, arr.length);
-			arr = temp;
-		}
-		//  3-2. 빈공간이 있으면 빈공간에 저장
-		arr[idx++] = vo;
+
+		list.add(new StudentVO(studentNo, studentName, major, score));
 	}
 
 	public void printAllStudent() {
